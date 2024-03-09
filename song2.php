f99d5c81307ed2f59d97a9986758d61b7de857ee
malacasmarc16@gmail.com <malacasmarc16@gmail.com>
2024-03-09 23:18:59 +0800

    Try lang

---
+++ song2.php
@@ -0,0 +1,12 @@
+<?php
+echo "<pre>";
+echo "\t      <strong>C</strong> \t\t\t   <strong>D</strong> <br>";
+echo "Darling, just dive right in, and follow my lead<br>";
+echo "\t\t<strong>G</strong>\t   <strong>Em</strong> <br>";
+echo "Well, I found a girl beautiful and sweet<br>";
+echo "When you're on it, you'll keep goin' down<br>";
+echo "And there's no one to hold on to<br>";
+echo "And there's no one to pull you out<br>";
+echo "You keep on fallin', no one can hear you callin'<br>";
+echo "</pre>"
+?>
